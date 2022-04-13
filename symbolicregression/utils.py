a4208974779e2109593297ef1992d0b14dd91b00
Pierre-Alexandre Kamienny <pakamienny@devfair0428.h2.fair>
2022-04-13 09:29:28 -0700

    first push

---
+++ symbolicregression/utils.py
@@ -0,0 +1,80 @@
+# Copyright (c) 2020-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+import os
+import re
+import sys
+import math
+import time
+import pickle
+import random
+import getpass
+import argparse
+import subprocess
+
+import errno
+import signal
+from functools import wraps, partial
+
+FALSY_STRINGS = {"off", "false", "0"}
+TRUTHY_STRINGS = {"on", "true", "1"}
+
+CUDA = True
+
+def bool_flag(s):
+    """
+    Parse boolean arguments from the command line.
+    """
+    if s.lower() in FALSY_STRINGS:
+        return False
+    elif s.lower() in TRUTHY_STRINGS:
+        return True
+    else:
+        raise argparse.ArgumentTypeError("Invalid value for a boolean flag!")
+
+def to_cuda(*args, use_cpu=False):
+    """
+    Move tensors to CUDA.
+    """
+    if not CUDA or use_cpu:
+        return args
+    return [None if x is None else x.cuda() for x in args]
+
+
+class MyTimeoutError(BaseException):
+    pass
+
+
+def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
+    def decorator(func):
+        def _handle_timeout(repeat_id, signum, frame):
+            # logger.warning(f"Catched the signal ({repeat_id}) Setting signal handler {repeat_id + 1}")
+            signal.signal(signal.SIGALRM, partial(_handle_timeout, repeat_id + 1))
+            signal.alarm(seconds)
+            raise MyTimeoutError(error_message)
+
+        def wrapper(*args, **kwargs):
+            old_signal = signal.signal(signal.SIGALRM, partial(_handle_timeout, 0))
+            old_time_left = signal.alarm(seconds)
+            assert type(old_time_left) is int and old_time_left >= 0
+            if 0 < old_time_left < seconds:  # do not exceed previous timer
+                signal.alarm(old_time_left)
+            start_time = time.time()
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                if old_time_left == 0:
+                    signal.alarm(0)
+                else:
+                    sub = time.time() - start_time
+                    signal.signal(signal.SIGALRM, old_signal)
+                    signal.alarm(max(0, math.ceil(old_time_left - sub)))
+            return result
+
+        return wraps(func)(wrapper)
+
+    return decorator
